13768eba8eddb0c5339196f73f5b836d38c03675
somyatuteja <somya16055@iiitd.ac.in>
2016-10-16 17:13:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/hp/fitfeed/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.hp.fitfeed;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       Button mRegisterButton =(Button)findViewById(R.id.signupButton);
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(getBaseContext(),RegisterActivity.class);
+                   startActivity(intent);
+            }
+        });
+
+
+    }
+}
