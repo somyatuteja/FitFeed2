d2e96f7768b2fd890c804d1e6210889b707aaebf
somyatuteja <somya16055@iiitd.ac.in>
2016-10-20 01:40:14 +0530

    update latest by somya

--- app/src/main/java/com/example/hp/fitfeed/HomeActivity.java
+++ app/src/main/java/com/example/hp/fitfeed/HomeActivity.java
@@ -2,6 +2,8 @@ package com.example.hp.fitfeed;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.main_menu,menu);
+        return true;
     }
 }
--- app/src/main/java/com/example/hp/fitfeed/MainActivity.java
+++ app/src/main/java/com/example/hp/fitfeed/MainActivity.java
@@ -7,11 +7,14 @@ import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
+
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -22,10 +25,14 @@ import com.google.firebase.auth.FirebaseAuth;
 public class MainActivity extends AppCompatActivity {
     void goToHomeActivity()
     {
+
+
         Intent intent = new Intent(getBaseContext(),HomeActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
         startActivity(intent);
         finish();
+
 
     }
 
@@ -39,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
          SharedPreferences sharedPreferences= getSharedPreferences("LoggedInUser", Context.MODE_PRIVATE);
         Log.v("MainActivity","jwebkjf");
+
         String user= sharedPreferences.getString("username",DEFAULT);
         Log.v("MainActivity","got preference : "+user);
         if(!user.equals(DEFAULT))
@@ -57,49 +65,47 @@ public class MainActivity extends AppCompatActivity {
         mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.v("MainActivity","login pressed");
-                String uname=mUnameEditText.getText().toString().trim();
-                String pass=mPassEditText.getText().toString().trim();
-                mAuth.signInWithEmailAndPassword(uname,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult>  task) {
-                        if(task.isSuccessful())
-                        {
-                            mProgress.setMessage("Signing In");
-                            mProgress.show();
+                Log.v("MainActivity", "login pressed");
+                String uname = mUnameEditText.getText().toString().trim();
+                String pass = mPassEditText.getText().toString().trim();
+                if (TextUtils.isEmpty(uname) || TextUtils.isEmpty(pass)) {
+                    Toast toast = Toast.makeText(getApplicationContext(), "Please enter the username and password", Toast.LENGTH_LONG);
+                    toast.show();
+                } else {
+                    mAuth.signInWithEmailAndPassword(uname, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                try {
 
-                            Log.v("MainActivity","successful");
-                            SharedPreferences sharedPreferences1=getSharedPreferences("LoggedInUser",Context.MODE_PRIVATE);
-                            SharedPreferences.Editor editor=sharedPreferences1.edit();
-                            Log.v("MainActivity","something");
-                            editor.putString("username",mUnameEditText.getText().toString());
-                            editor.commit();
-                            Log.v("MainActivity","blah");
-                            goToHomeActivity();
-                            Log.v("MainActivity","dhfjshfkjsdhkjshd");
+                                    mProgress.setMessage("Signing In");
+                                    mProgress.show();
+                                }
+                                catch (Exception e)
+                                {
+                                    Log.v("","this is error1");
+                                }
+                                Log.v("MainActivity", "successful");
+                                SharedPreferences sharedPreferences1 = getSharedPreferences("LoggedInUser", Context.MODE_PRIVATE);
+                                SharedPreferences.Editor editor = sharedPreferences1.edit();
+                                Log.v("MainActivity", "something");
+                                editor.putString("username", mUnameEditText.getText().toString());
+                                editor.commit();
+                                Toast toast = Toast.makeText(getApplicationContext(), "Please enter the username and password", Toast.LENGTH_LONG);
+                                toast.show();
+                                Log.v("MainActivity", "blah");
+                                mProgress.dismiss();
+                                goToHomeActivity();
 
-
+                            }
+                            if (task.isComplete()) {
+                                Log.v("MainActivity", "complete");
+                            } else Log.v("MainActivity", "bakwaas");
                         }
-                        if(task.isComplete())
-                        {
-                            Log.v("MainActivity","complete");
-
-                        }
-                        else
-                            Log.v("MainActivity","bakwaas");
-
-                        if(task.isComplete())
-                        {
-                            Log.v("MainActivity","complete");
-
-                        }
-                        else
-                            Log.v("MainActivity","bakwaas");
-                    }
-                });
+                    });
+                }
             }
         });
-
 
         mRegisterButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -111,4 +117,6 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
 }
