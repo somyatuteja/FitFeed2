c9383f5f4f344062fc06f056415d5a7e992858df
somyatuteja <somya16055@iiitd.ac.in>
2016-11-28 17:32:38 +0530

    latest by somya 2

---
+++ app/src/main/java/com/example/hp/fitfeed/DbHelper.java
@@ -0,0 +1,27 @@
+package com.example.hp.fitfeed;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
+
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+
+public class DbHelper extends SQLiteAssetHelper {
+   private static String DB_NAME= "Database.db";
+    private static String TableName="FoodTable";
+    public DbHelper(Context context) {
+        super(context,DB_NAME , null, 1);
+    }
+    public Cursor getData() {
+        SQLiteDatabase db = getReadableDatabase();
+        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+        String [] sqlSelect = {"'Food Name'"};
+        qb.setTables(TableName);
+        Cursor c = qb.query(db, sqlSelect, null, null,
+                null, null, null);
+                c.moveToFirst();
+                 return c;
+
+    }
+}
